dd9a71cf308e7c2bc5e6f53596efc504f9533969
aishatpopoola <aishatpopoola906@gmail.com>
2022-10-03 00:02:20 +0100

    feat:add little styling to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes,Route } from "react-router-dom";
-import "./App.css";
 import Layout from "./component/Layout";
 import CreateEvent from "./pages/CreateEvent/CreateEvent";
 import EventPage from "./pages/EventPage/EventPage";
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from "react";
+import './home.css';
 
  const Home = () => {
      return ( 
-         <h1>imagine if Snapchat had events</h1> 
+         <main className="main">
+            <h1 className="home-text">Imagine if Snapchat had events.</h1>
+            <p className="home-text">Easily host and share events with your friends across any social media.</p> 
+         </main>
+         
       );
  }
   
